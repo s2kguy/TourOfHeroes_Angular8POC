ca2adf9fbbade1989796f483dadda940017939d3
s2kguy <ryansblum@gmail.com>
2020-02-08 10:18:09 -0500

    Initial Commit

---
+++ src/app/Components/hero-search/hero-search.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+
+import {
+   debounceTime, distinctUntilChanged, switchMap
+ } from 'rxjs/operators';
+
+import { Hero } from '../../Models/hero';
+import { HeroService } from '../../Services/hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: [ './hero-search.component.css' ]
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroService: HeroService) {}
+
+  // Push a search term into the observable stream.
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      // debounceTime(300) waits until the flow of new
+      // string events pauses for 300 milliseconds before
+      // passing along the latest string. You'll never
+      // make requests more frequently than 300ms.
+      debounceTime(300),
+
+      // distinctUntilChanged() ensures that a request
+      // is sent only if the filter text changed.
+      // AKA: ignore new term if same as previous term
+      distinctUntilChanged(),
+
+      //  witchMap() calls the search service for each search term
+      //  that makes it through debounce() and distinctUntilChanged().
+      //  It cancels and discards previous search observables,
+      //  returning only the latest search service observable.
+      //  AKA: switch to new search observable each time the term changes
+      switchMap((term: string) => this.heroService.searchHeroes(term)),
+    );
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './Services/in-memory-data.service';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './Components/heroes/heroes.component';
+import { HeroDetailComponent } from './Components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './Components/messages/messages.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { AppRoutingModule} from './app-routing.module';
+import { HeroSearchComponent } from './Components/hero-search/hero-search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+    HeroSearchComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
